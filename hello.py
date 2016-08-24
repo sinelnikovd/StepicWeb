f177987665de0420a662fb764d6d5c01ff9f98f1
sinelnikovd <sinelnikovdmitriy@yandex.ru>
2016-08-24 23:03:27 +0600

    hello.py-3

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-bind = "127.0.0.1:8080"
 def app(environ, start_response):
-	data = "Hello"
 	status = '200 OK'
 	response_headers = [
 		('Content-type','text/plain')
 	]
+	data = environ['QUERY_STRING'].replace('&','\n')
 	start_response(status, response_headers)
 	return [data]
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #! usr/bin/bash
-sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 ﻿#sudo /etc/init.d/mysql start﻿
