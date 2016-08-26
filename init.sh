4a4cf111cd33115c11c9b4e4cb7e11e5114107aa
sinelnikovd <sinelnikovdmitriy@yandex.ru>
2016-08-26 16:16:58 +0600

    django conf init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
+sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
