4842149c9a9a80bfa21d581db75c5ecae32c1434
sinelnikovd <sinelnikovdmitriy@yandex.ru>
2016-09-12 01:50:37 +0600

    lesons 2.3.2

--- init.sh
+++ init.sh
@@ -5,3 +5,8 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.conf
 sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql restart
+mysql -uroot -e "CREATE USER 'dj'@'%' IDENTIFIED BY '';"
+mysql -uroot -e "GRANT ALL ON *.* TO 'dj'@'%';"
+mysql -uroot -e "СREATE DATABASE django;"
+python ask/manager.py migrate
